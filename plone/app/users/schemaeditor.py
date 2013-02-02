ed5c95fc1c1b4478714d7be812da99bfef72a1d0
Eric BREHAULT <ebrehault@gmail.com>
2013-02-02 16:49:07 +0100

    update portal_memberdata

--- plone/app/users/schemaeditor.py
+++ plone/app/users/schemaeditor.py
@@ -8,6 +8,7 @@ from zope.schema.interfaces import IField
 from plone.schemaeditor.browser.schema.traversal import SchemaContext
 from plone.schemaeditor.interfaces import IFieldEditorExtender
 from Products.CMFPlone import PloneMessageFactory as _
+from Products.CMFCore.utils import getToolByName
 
 from userdataschema import IUserDataSchemaProvider, SCHEMA_ANNOTATION
 
@@ -72,4 +73,38 @@ class MemberSchemaContext(SchemaContext):
 def updateSchema(object, event):
     site = getSite()
     annotations = IAnnotations(site)
-    annotations[SCHEMA_ANNOTATION] = object.schema._InterfaceClass__attrs
+    old_schema = annotations.get(SCHEMA_ANNOTATION, {})
+    new_schema = object.schema._InterfaceClass__attrs
+
+    # store the extra schema in an annotation
+    annotations[SCHEMA_ANNOTATION] = new_schema
+
+    # update portal_memberdata properties
+    pm = getToolByName(site, "portal_memberdata")
+    field_type_mapping = {
+        "TextLine": 'text',
+        "Text": 'lines',
+        "Int": 'int',
+        "Float": 'float',
+        "Bool": 'boolean',
+        "Datetime": 'date',
+        "Date": 'date',
+        "Choice": 'text',
+        "List": 'text',
+    }
+
+    existing = pm.propertyIds()
+    for field_id in new_schema.keys():
+        field_type = field_type_mapping.get(
+            new_schema[field_id].__class__.__name__,
+            None)
+        if not field_type:
+            continue
+        if field_id in existing:
+            pm._delProperty(field_id)
+        pm._setProperty(field_id, '', field_type)
+
+    to_remove = [field_id for field_id in old_schema.keys()
+                    if field_id not in new_schema.keys()]
+    for field_id in to_remove:
+        pm._delProperty(field_id)
