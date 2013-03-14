aaba4e4d029f8237ba8d64b215edd47c57da2de1
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2013-03-14 19:36:48 +0100

    Disable fields which are not created TTW & control actions

--- plone/app/users/browser/listing.py
+++ plone/app/users/browser/listing.py
@@ -14,16 +14,48 @@ from plone.schemaeditor.interfaces import IFieldFactory
 from plone.schemaeditor.utils import SchemaModifiedEvent
 
 from plone.schemaeditor.browser.schema import listing
+from plone.schemaeditor.browser.field import order
 from plone.protect import CheckAuthenticator
 from ZTUtils import make_query
+from ..schemaeditor import get_ttw_edited_schema
+
+class NotEditableField(Exception):pass
+
 
 class SchemaListing(listing.SchemaListing):
     template = ViewPageTemplateFile('schema_listing.pt')
- 
+
     @button.buttonAndHandler(_(u'Save Defaults'))
     def handleSaveDefaults(self, action):
         CheckAuthenticator(self.request)
         listing.SchemaListing.handleSaveDefaults(self, action)
+
+    def render(self):
+        fields = get_ttw_edited_schema()
+        for widget in self._iterateOverWidgets():
+            # disable fields from behaviors
+            if widget.field.__name__ not in fields:
+                widget.disabled = 'disabled'
+        return super(listing.SchemaListing, self).render()
+
+    def field_classes(self, field):
+        classes = ['fieldPreview']
+        fields = get_ttw_edited_schema()
+        if field.__name__ in fields:
+            classes.append('orderable')
+        return ' '.join(classes)
+
+
+    def edit_url(self, field):
+        fields = get_ttw_edited_schema()
+        if field.__name__ in fields:
+            return super(SchemaListing, self).edit_url(field)
+
+    def delete_url(self, field):
+        fields = get_ttw_edited_schema()
+        if field.__name__ in fields:
+            return '%s/%s/@@memberfield_delete' % (self.context.absolute_url(),
+                                                   field.__name__)
 
 class ReadOnlySchemaListing(SchemaListing):pass
 
@@ -35,3 +67,18 @@ class SchemaListingPage(listing.SchemaListingPage):
 
     def makeQuery(self, **kw):
         return make_query(**kw)
+
+class FieldOrderView(order.FieldOrderView):
+    def checkEditableField(self):
+        fields = get_ttw_edited_schema()
+        if not self.field.__name__ in fields:
+            raise NotEditableField("Default field, not editable")
+
+    def move(self, pos):
+        self.checkEditableField()
+        super(FieldOrderView, self).move(pos)
+
+    def delete(self):
+        self.checkEditableField()
+        super(FieldOrderView, self).delete()
+
