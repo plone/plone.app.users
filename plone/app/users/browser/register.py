e806182e8ed92dbdbf0e32af6eed552bd6faa2c4
Vincent Fretin <vincent.fretin@gmail.com>
2013-06-24 08:22:35 +0200

    Translating the msg here is not needed, the template takes care of that.

--- plone/app/users/browser/register.py
+++ plone/app/users/browser/register.py
@@ -249,8 +249,8 @@ class BaseRegistrationForm(PageForm):
             registration = getToolByName(self.context, 'portal_registration')
             err_str = registration.testPasswordValidity('')
             if err_str:
-                msgid = _(u'Enter your new password. ${errors}', mapping=dict(errors=err_str))
-                all_fields['password'].field.description = self.context.translate(msgid)
+                msg = _(u'Enter your new password. ${errors}', mapping=dict(errors=err_str))
+                all_fields['password'].field.description = msg
 
         # Pass the list of join form fields as a reference to the
         # Fields constructor, and return.
