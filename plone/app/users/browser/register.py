03f9f44394fb042a1352f6acdf5308b50eeddb66
Johannes Raggam <raggam-nl@adm.at>
2013-10-28 12:23:17 +0100

    mv registrationcontrolpanel from registration to register

--- plone/app/users/browser/register.py
+++ plone/app/users/browser/register.py
@@ -31,6 +31,8 @@ from zope.schema import getFieldNames
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 from zope.site.hooks import getSite
 import logging
+from plone.app.users.registrationschema import IRegistrationSchema
+from z3c.form.browser.orderedselect import OrderedSelectFieldWidget
 
 
 # Define constants from the Join schema that should be added to the
@@ -39,6 +41,9 @@ JOIN_CONST = ['username', 'password', 'email', 'mail_me']
 
 # Number of retries for creating a user id like bob-jones-42:
 RENAME_AFTER_CREATION_ATTEMPTS = 100
+
+# Property as it is named in portal_properties
+USER_REGISTRATION_FIELDS = 'user_registration_fields'
 
 
 class IRegisterSchema(Interface):
@@ -916,3 +921,61 @@ class AddUserForm(BaseRegistrationForm):
             '/@@usergroup-userprefs?searchstring=' + user_id)
 
         self._finishedRegister = True
+
+
+class RegistrationControlPanel(form.Form):
+    label = _(u"Registration settings")
+    description = _(u"Registration settings for this site.")
+    form_name = _(u"Registration settings")
+
+    formErrorsMessage = _('There were errors.')
+    template = ViewPageTemplateFile('memberregistration.pt')
+
+    fields = field.Fields(IRegistrationSchema)
+    fields['user_registration_fields'].widgetFactory = OrderedSelectFieldWidget
+
+    def getContent(self):
+        props = self.props()
+        return {'user_registration_fields': props.getProperty(
+            USER_REGISTRATION_FIELDS, [])}
+
+    @button.buttonAndHandler(_(u'label_save', default=u'Save'), name='save')
+    def action_save(self, action):
+        # CSRF protection
+        CheckAuthenticator(self.request)
+
+        data, errors = self.extractData()
+        if errors:
+            IStatusMessage(self.request).addStatusMessage(
+                self.formErrorsMessage, type='error')
+            return
+
+        # save property
+        if data['user_registration_fields'] != \
+                self.getContent()['user_registration_fields']:
+            props = self.props()
+            props._updateProperty(
+                USER_REGISTRATION_FIELDS,
+                data['user_registration_fields']
+            )
+            msg = _("Changes saved.")
+        else:
+            msg = _("No changes made.")
+        IStatusMessage(self.request).addStatusMessage(msg, type="info")
+
+    @button.buttonAndHandler(
+        _(u'label_cancel', default=u'Cancel'), name='cancel'
+    )
+    def action_cancel(self, action):
+        IStatusMessage(self.request).addStatusMessage(
+            _("Changes canceled."), type="info"
+        )
+        url = getMultiAdapter(
+            (self.context, self.request),
+            name='absolute_url'
+        )()
+        self.request.response.redirect(url + '/plone_control_panel')
+
+    def props(self):
+        pprop = getToolByName(self.context, 'portal_properties')
+        return pprop.site_properties
