97ee428d2d241f96d28a975a8a8a49a4afe1cb0c
Kees Hink <keeshink@gmail.com>
2013-11-13 14:47:12 +0100

    Remove unused validate method again

--- plone/app/users/browser/userdatapanel.py
+++ plone/app/users/browser/userdatapanel.py
@@ -64,52 +64,6 @@ class UserDataPanel(AccountPanelForm):
     form_name = _(u'User Data Form')
     schema = IUserDataSchema
 
-    def validate(self, errors, data):
-        context = aq_inner(self.context)
-        error_keys = [error.field.getName() for error in errors]
-
-        if 'email' not in error_keys:
-            props = getToolByName(context, 'portal_properties')
-            if props.site_properties.getProperty('use_email_as_login'):
-                # Keeping your email the same (which happens when you
-                # change something else on the personalize form) or
-                # changing it back to your original user id, is fine.
-                membership = getToolByName(context, 'portal_membership')
-                if self.userid:
-                    member = membership.getMemberById(self.userid)
-                else:
-                    member = membership.getAuthenticatedMember()
-                email = data['email']
-                pas = getToolByName(context, 'acl_users')
-                email = pas.applyTransform(email)
-                if email not in (member.getId(), member.getUserName()):
-                    # Our email has changed and is not the same as our
-                    # user id or login name, so we need to check if
-                    # this email is already in use by another user.
-                    pas = getToolByName(context, 'acl_users')
-                    if (membership.getMemberById(email) or
-                            pas.searchUsers(name=email, exact_match=True)):
-                        err_str = _(
-                            'message_email_in_use',
-                            default=(
-                                u"The email address you selected is "
-                                u"already in use or is not valid as login "
-                                u"name. Please choose another."))
-                        widget = self.widgets['email']
-                        err_view = getMultiAdapter(
-                            (Invalid(err_str),
-                             self.request,
-                             widget,
-                             widget.field,
-                             self,
-                             self.context), IErrorViewSnippet)
-                        err_view.update()
-                        widget.error = err_view
-                        self.widgets.errors += (err_view,)
-                        errors += (err_view,)
-
-        return errors
-
     @property
     def description(self):
         userid = self.request.form.get('userid')
