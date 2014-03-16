8b28b512a60285754f00cbbebc533ec557da6236
Ian Anderson <ianderso@med.unc.edu>
2014-03-15 22:55:30 -0700

    removal of old shcema elements

--- plone/app/users/schema.py
+++ plone/app/users/schema.py
@@ -19,6 +19,7 @@ SCHEMA_ANNOTATION = "plone.app.users.schema"
 # must match the browser view name !
 SCHEMATA_KEY = "member-fields"
 
+
 def checkEmailAddress(value):
     portal = getUtility(ISiteRoot)
 
@@ -40,42 +41,6 @@ class IUserDataSchema(Interface):
         description=_(u'help_full_name_creation',
                       default=u"Enter full name, e.g. John Smith."),
         required=False)
-
-
-
-#     home_page = schema.TextLine(
-#         title=_(u'label_homepage', default=u'Home page'),
-#         description=_(u'help_homepage',
-#                       default=u"The URL for your external home page, "
-#                       "if you have one."),
-#         required=False)
-#
-#     description = schema.Text(
-#         title=_(u'label_biography', default=u'Biography'),
-#         description=_(u'help_biography',
-#                       default=u"A short overview of who you are and what you "
-#                       "do. Will be displayed on your author page, linked "
-#                       "from the items you create."),
-#         required=False)
-#
-#     location = schema.TextLine(
-#         title=_(u'label_location', default=u'Location'),
-#         description=_(u'help_location',
-#                       default=u"Your location - either city and "
-#                       "country - or in a company setting, where "
-#                       "your office is located."),
-#         required=False)
-#
-#     portrait = NamedBlobImage(
-#         title=_(u'label_portrait', default=u'Portrait'),
-#         description=_(
-#             u'help_portrait',
-#             default=u'To add or change the portrait: click the "Browse" '
-#                     u'button; select a picture of yourself.  Recommended '
-#                     u'image size is 75 pixels wide by 100 pixels tall.'
-#         ),
-#         required=False)
-#     form.widget(portrait='plone.app.users.schema.PortraitFieldWidget')
 
 
 class IRegisterSchema(Interface):
