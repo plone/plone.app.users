9e68eeed532858866b1524a77a3b3dba3434cf2a
Gil Forcada <gil.forcada@freitag.de>
2014-07-28 13:55:54 +0200

    Rename tests from .txt to .rst

--- plone/app/users/tests/test_doctests.py
+++ plone/app/users/tests/test_doctests.py
@@ -10,28 +10,29 @@ import doctest
 setupPloneSite()
 OPTIONFLAGS = (doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE)
 
+doc_tests = [
+    'duplicate_email.rst',
+    'email_login.rst',
+    'flexible_user_registration.rst',
+    'forms_navigationroot.rst',
+    'member_search.rst',
+    'registration_forms.rst',
+    'password.rst',
+    'personal_preferences.rst',
+    'personal_preferences_prefs_user_details.rst',
+    'userdata.rst',
+    'userdata_prefs_user_details.rst',
+    '../vocabularies.py',
+]
+
 
 def test_suite():
-    tests = [
-        'duplicate_email.txt',
-        'flexible_user_registration.txt',
-        'forms_navigationroot.txt',
-        'registration_forms.txt',
-        'userdata.txt',
-        'userdata_prefs_user_details.txt',
-        'personal_preferences.txt',
-        'personal_preferences_prefs_user_details.txt',
-        'password.txt',
-        'email_login.txt',
-        'member_search.txt',
-        '../vocabularies.py',
-        ]
     suite = TestSuite()
-    for test in tests:
+    for test_file in doc_tests:
         suite.addTest(FunctionalDocFileSuite(
-            test,
+            test_file,
             optionflags=OPTIONFLAGS,
-            package="plone.app.users.tests",
+            package='plone.app.users.tests',
             test_class=BaseTestCase
         ))
     return suite
