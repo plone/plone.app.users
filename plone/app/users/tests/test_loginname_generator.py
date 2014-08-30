2bd9bd5027c73b7f2a25f8966a3a4792411eac69
Gil Forcada <gil.forcada@freitag.de>
2014-08-30 23:12:29 +0200

    Migrate test_loginname_generator.py

--- plone/app/users/tests/test_loginname_generator.py
+++ plone/app/users/tests/test_loginname_generator.py
@@ -1,18 +1,25 @@
 # -*- coding: utf-8 -*-
-# Note: test setup somehow fails when only tests from this file are run.
 from plone.app.users.browser.interfaces import ILoginNameGenerator
 from plone.app.users.browser.register import BaseRegistrationForm
-from plone.app.users.tests.base import BaseTestCase
+from plone.app.users.testing import PLONE_APP_USERS_INTEGRATION_TESTING
 from zope.component import getSiteManager
 
+import unittest
 
-class TestGenerateLoginName(BaseTestCase):
+
+class TestGenerateLoginName(unittest.TestCase):
+
+    layer = PLONE_APP_USERS_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
     def test_generate_user_id_simplistic(self):
         sm = getSiteManager(context=self.portal)
 
         # Without a function, return username
         self.assertEqual(
-            self.generateLoginName(dict(username='frank')),
+            self._generate_login_name(dict(username='frank')),
             'frank'
         )
 
@@ -20,11 +27,12 @@ class TestGenerateLoginName(BaseTestCase):
         sm.registerUtility(lambda data: data['useme'],
                            provided=ILoginNameGenerator)
         self.assertEqual(
-            self.generateLoginName(dict(useme='me me me', username='frank')),
+            self._generate_login_name(dict(useme='me me me',
+                                           username='frank')),
             'me me me'
         )
 
-    def generateLoginName(self, data):
+    def _generate_login_name(self, data):
         """Generate login name, optionally registering function first"""
         form = BaseRegistrationForm(self.portal, {})
         return form.generate_login_name(data)
