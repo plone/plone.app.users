06875bd152c340f466a01a6afc7dc5c70d95c6f8
Eric BREHAULT <ebrehault@gmail.com>
2015-01-09 00:11:05 +0100

    add form selection serialization

--- plone/app/users/field_extender.py
+++ plone/app/users/field_extender.py
@@ -1,13 +1,16 @@
-from zope.interface import Interface
+from zope.interface import Interface, implements
 from zope import schema
+from zope.component import provideUtility
 
 from zope.component import provideAdapter, adapter, adapts
 from zope.schema.interfaces import IField
 from plone.schemaeditor.interfaces import ISchemaContext
+from plone.schemaeditor.interfaces import IFieldEditorExtender
+from plone.supermodel.interfaces import IFieldMetadataHandler
+from plone.supermodel.utils import ns
 
 from .browser.schemaeditor import IMemberSchemaContext
-
-from plone.schemaeditor.interfaces import IFieldEditorExtender
+from .browser.schemaeditor import USERS_NAMESPACE, USERS_PREFIX
 
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
 
@@ -49,13 +52,35 @@ class UserFormSelectionAdapter(object):
         self.field = field
 
     def _get_forms(self):
-        forms = self.field.interface.queryTaggedValue('user_form_selection', {})
+        forms = self.field.interface.queryTaggedValue(u'forms.selection', {})
         return forms.get(self.field.__name__)
     def _set_forms(self, value):
-        forms = self.field.interface.queryTaggedValue('user_form_selection', {})
+        forms = self.field.interface.queryTaggedValue(u'forms.selection', {})
         forms[self.field.__name__] = value
-        self.field.interface.setTaggedValue('user_form_selection', forms)
+        self.field.interface.setTaggedValue(u'forms.selection', forms)
     forms = property(_get_forms, _set_forms)
 
 provideAdapter(UserFormSelectionAdapter, provides=IUserFormSelection)
 
+
+class UserFormSelectionMetadata(object):
+    implements(IFieldMetadataHandler)
+
+    namespace = USERS_NAMESPACE
+    prefix = USERS_PREFIX
+
+    def read(self, fieldNode, schema, field):
+        name = field.__name__
+        forms = fieldNode.get(ns('forms', self.namespace))
+        if forms:
+            data = schema.queryTaggedValue(u'forms.selection', {})
+            data[name] = forms
+            schema.setTaggedValue(u'forms.selection', data)
+
+    def write(self, fieldNode, schema, field):
+        name = field.__name__
+        forms = schema.queryTaggedValue(u'forms.selection', {}).get(name, {})
+        if forms:
+            fieldNode.set(ns('forms', self.namespace), forms)
+
+provideUtility(component=UserFormSelectionMetadata(), name='plone.app.users.forms')
