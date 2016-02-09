8967925ec26129e184ade5c2b2c8d8def0c9366c
Maurits van Rees <maurits@vanrees.org>
2016-02-09 23:05:37 +0100

    Used https in pypi url.

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     keywords='Zope CMF Plone Users Groups',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='http://pypi.python.org/pypi/plone.app.users',
+    url='https://pypi.python.org/pypi/plone.app.users',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone', 'plone.app'],
