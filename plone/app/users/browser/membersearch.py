1691bc729cc95a3d3da74deb80c966315c9b1b9b
kaif khan <khanstark91@gmail.com>
2016-04-08 02:22:24 +0530

    Update membersearch.py

--- plone/app/users/browser/membersearch.py
+++ plone/app/users/browser/membersearch.py
@@ -23,14 +23,14 @@ class IMemberSearchSchema(model.Schema):
         title=_(u'label_name', default=u'Name'),
         description=_(
             u'help_search_name',
-            default=u'Find users whose login names contain.'),
+            default=u'Find users with login name'),
         required=False,
     )
     email = schema.TextLine(
         title=_(u'label_email', default=u'E-mail'),
         description=_(
             u'help_search_email',
-            default=u'Find users whose email addresses contain.'),
+            default=u'Find users with email address'),
         constraint=checkEmailAddress,
         required=False,
     )
@@ -38,7 +38,7 @@ class IMemberSearchSchema(model.Schema):
         title=_(u'label_fullname', default=u'Full Name'),
         description=_(
             u'help_search_fullname',
-            default=u'Return users with full names containing this value.'),
+            default=u'Find users with full names'),
         required=False,
     )
     # disabled: https://dev.plone.org/ticket/13862
