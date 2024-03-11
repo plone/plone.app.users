7591ed617e7ee0540c980b0e92741a6b9cc7469d
Maurits van Rees <maurits@vanrees.org>
2024-03-11 14:44:11 +0100

    black and codespell

--- plone/app/users/tests/base.py
+++ plone/app/users/tests/base.py
@@ -4,6 +4,7 @@ This is in a separate module because it's potentially useful to other
 packages which register accountpanels. They should be able to import it
 without the PloneTestCase.setupPloneSite() side effects.
 """
+
 from AccessControl.SecurityInfo import ClassSecurityInfo
 from Acquisition import aq_base
 from OFS.Cache import Cacheable
