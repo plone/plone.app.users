c0fc3c131159b16d1fd0dc8fdbbb85dd07758c83
Maurits van Rees <maurits@vanrees.org>
2025-11-19 11:02:53 +0100

    Preparing release 4.0.0a1
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from setuptools import setup
 
 
-version = "4.0.0a1.dev0"
+version = "4.0.0a1"
 
 long_description = (
     f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}"
