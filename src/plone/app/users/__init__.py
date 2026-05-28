266040662fe68f459b349ac128ca1d6699999884
Thomas Lotze <lotze@starzel.de>
2026-05-28 16:42:26 +0200

    move AccountPanelForm to plone.app.layout

--- src/plone/app/users/__init__.py
+++ src/plone/app/users/__init__.py
@@ -1,3 +1,4 @@
 jbot_deprecations = {
+    "plone.app.users.browser.account-panel.pt": "plone.app.layout.users.account-panel.pt",
     "plone.app.users.browser.schema_layout.pt": "plone.app.layout.users.schema_layout.pt",
 }
